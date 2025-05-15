f2a128f07affc0ab833491ef2be5312cb1f3604f
Connor McCarl <connormccarl@gmail.com>
2025-05-15 12:13:27 -0700

    added some breaklines to my main company address

--- creativcodes-app/src/app/page.tsx
+++ creativcodes-app/src/app/page.tsx
@@ -259,7 +259,7 @@ export default function Home() {
                         </li>
                         <li className="text-center" >
                           <div className="feature-small bg-primary bg-gradient text-white rounded-3 me-3"><i className="bi bi-geo-alt-fill"></i></div>
-                          26191 La Real, Apt E, Mission Viejo, CA 92691
+                          26191 La Real<br />Apt E<br />Mission Viejo, CA 92691
                         </li>
                       </ul>
                     </div>
